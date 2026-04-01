37d7c183bd95ac9329b220bf0f2598e0d8f64b23
vishesh singhal <vishesh.singhal@myyahoo.com>
2026-04-01 11:14:38 +0530

    Initial project setup with activities and layouts

--- CameraGalleryApp/app/src/main/java/com/example/cameragalleryapp/MainActivity.java
+++ CameraGalleryApp/app/src/main/java/com/example/cameragalleryapp/MainActivity.java
@@ -1,24 +1,37 @@
-package com.example.cameragalleryapp;
+package com.example.photogalleryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCamera, btnGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnCamera = findViewById(R.id.btnCamera);
+        btnGallery = findViewById(R.id.btnGallery);
+
+        btnCamera.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivity(intent);
+
         });
+
+        btnGallery.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+            startActivity(intent);
+
+        });
+
     }
 }
